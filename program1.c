d6f748ffd632030f2f230fb3e5b0114c3927ac9f
Avinash Kumar <61144559+avinashkumar-rakuten@users.noreply.github.com>
2020-02-19 09:57:20 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int a,n;
+printf("key in the n value for which square root to be found out:\n");
+scanf("%d",&n);
+a=sqrt(n);
+printf("the square root of n= %d is %d\n",n,a);
+}
+
